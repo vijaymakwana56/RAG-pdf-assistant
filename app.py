b22e75b8586925cc0bb58852ffd86839d42d589f
Vijay Makwana <vijaygm20@iitk.ac.in>
2026-03-09 22:16:10 +0530

    feat: connected to the ui

--- app.py
+++ app.py
@@ -1,16 +1,64 @@
+import os
+import streamlit as st
 from src.data_loader import load_all_documents
-# from src.search import RAGSearch
+from src.pinecone_chunking import DataChunking
+from src.pinecone_database import PineconeVectorstore
 from src.search_on_pinecone import PRAGSearch
 
+st.title("Chat With Your PDFs")
 
-if __name__ == "__main__": 
-    # docs = load_all_documents('pdfs')
-    # store = FaissVectorStore("faiss_store")
-    # # store.build_from_documents(docs)
-    # store.load()
-    # print(store.query("what is encoder-decoder architecture?",top_k=3))
-    query = "what is encoder-decoder architecture?"
-    # rag_search = RAGSearch()
-    rag_search = PRAGSearch()
-    summary = rag_search.search_and_summarize(userID="user1",query=query,top_k=3)
-    print(f"summary: {summary}")
+upload_dir = "pdfs"
+os.makedirs(upload_dir, exist_ok=True)
+
+if "rag" not in st.session_state:
+    st.session_state.rag = PRAGSearch()
+
+ck = DataChunking()
+db = PineconeVectorstore()
+
+userId = "user2"
+
+#-----------PDF Uploader----------
+uploaded_file = st.file_uploader("Upload your PDF", type=["pdf"])
+
+if uploaded_file is not None:
+    save_path = os.path.join(upload_dir, uploaded_file.name)
+
+    with open(save_path,"wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    st.success("PDF Uploaded Sucessfully")
+
+    #load documents
+    docs = load_all_documents(upload_dir)
+
+    #creating chunks
+    chunks = ck.chunk_documents(documents=docs)
+
+    #sotre into pinecone database
+    db.upsert_chunks(
+        userID=userId,
+        document_name=uploaded_file.name,
+        chunks=chunks
+    )
+
+    st.success("Documents processed and stored.")
+
+
+#----------Chat Section----------
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+prompt = st.chat_input("Ask a question about your document")
+
+if prompt:
+    st.session_state.messages.append({"role":"user", "content":prompt})
+
+    answer = st.session_state.rag.search_and_summarize(userID=userId,query=prompt)
+
+    st.session_state.messages.append({"role":"assistant", "content":answer})
+
+
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.write(msg["content"])
---
+++ local_app.py
@@ -0,0 +1,18 @@
+from src.data_loader import load_all_documents
+# from src.search import RAGSearch
+from src.search_on_pinecone import PRAGSearch
+
+
+
+if __name__ == "__main__": 
+# for local vectorstore use these next 6 lines:
+    # docs = load_all_documents('pdfs')
+    # store = FaissVectorStore("faiss_store")
+    # store.build_from_documents(docs)
+    # store.load()
+    # print(store.query("what is encoder-decoder architecture?",top_k=3))
+    # rag_search = RAGSearch()
+    query = "what is encoder-decoder architecture?"
+    rag_search = PRAGSearch()
+    summary = rag_search.search_and_summarize(userID="user1",query=query,top_k=3)
+    print(f"summary: {summary}")
